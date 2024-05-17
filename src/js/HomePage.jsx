1c906241dab9be00417b5000b67f526b66f417a2
Jadude <160776823+Jadude@users.noreply.github.com>
2024-05-17 11:21:39 +0200

    Environment works

--- src/js/HomePage.jsx
+++ src/js/HomePage.jsx
@@ -2,13 +2,13 @@ import React from 'react';
 
 const Home = () => {
     return (
-        <>
-            <div style {{background: "yellow", width: "100px", height: "100px"}}>Add product</div>
-            <div style {{background: "green", width: "100px", height: "100px"}}>My products</div>
-            <div style {{background: "crimson", width: "100px", height: "100px"}}>Fave</div>
-            <div style {{background: "lime", width: "100px", height: "100px"}}>My recipes</div>
-            <div style {{background: "brown", width: "100px", height: "100px"}}>Tip!</div>
-        </>
+        <div style={{display: 'flex'}}>
+            <div style={{background: "yellow", width: "100px", height: "100px"}}>Add product</div>
+            <div style={{background: "green", width: "100px", height: "100px"}}>My products</div>
+            <div style={{background: "crimson", width: "100px", height: "100px"}}>Fave</div>
+            <div style={{background: "lime", width: "100px", height: "100px"}}>My recipes</div>
+            <div style={{background: "brown", width: "100px", height: "100px"}}>Tip!</div>
+        </div>
     );
 };
 
